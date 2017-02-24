9b6902a9b7215a88415603521536144a1b7fb0dc
Kat Guthrie <kathrynguthrie@gmail.com>
2017-02-24 11:41:23 -0500

    added dog model

---
+++ db/models/dog.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Sequelize = require('sequelize')
+const db = require('APP/db')
+
+const Dog = db.define('dogs', {
+  name: Sequelize.STRING, 
+  breed: Sequelize.STRING,
+  age: Sequelize.INTEGER,
+  parentName: Sequelize.STRING,
+  preferredPettings: Sequelize.ARRAY,
+  okToFeed: Sequelize.TEXT,
+  notes: Sequelize.TEXT,
+  picture: Sequelize.STRING,
+  floor: Sequelize.ENUM(11,25),
+  cohort: Sequelize.STRING
+} 
+
+module.exports = Dog;
--- db/models/index.js
+++ db/models/index.js
@@ -1,12 +1,5 @@
 'use strict';
 
-// Require our models. Running each module registers the model into sequelize
-// so any other part of the application could call sequelize.model('User')
-// to get access to the User model.
+const Dog = require('./dog')
 
-const User = require('./user')
-const OAuth = require('./oauth')
-
-OAuth.belongsTo(User)
-User.hasOne(OAuth)
-module.exports = {User}
+module.exports = Dog;
